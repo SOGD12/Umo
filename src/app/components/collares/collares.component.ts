d01814042d5d3c0c77010f25419a46367d7d6a79
SOGD12 <sosoriog@siesa.com>
2024-01-04 16:38:08 -0500

    Umo version 1.1.2

--- src/app/components/collares/collares.component.ts
+++ src/app/components/collares/collares.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AretesService , Collares } from '../../service/aretes.service' ;
 
 @Component({
   selector: 'app-collares',
   templateUrl: './collares.component.html',
   styleUrls: []
 })
-export class CollaresComponent {
+export class CollaresComponent implements OnInit {
+  collares:Collares[] = [];
 
+
+  constructor( private  _CollaresService: AretesService){
+
+  }
+
+  ngOnInit(){
+    this.collares = this._CollaresService.getCollares();
+  }
 }
